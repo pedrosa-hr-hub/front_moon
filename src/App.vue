491acfed77ece9bfeb078d8ccb0aca9aedbd668e
Pedro de Sá <pedro.trabalhos@outlook.com>
2022-10-07 22:59:27 -0300

    initial commit, uikit is config!

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+UIkit.use(Icons);
+
+export default {
+
+  name: 'App',
+  
+};
+
+</script>
+
+<style lang="less">
+
+@import "../node_modules/uikit/src/less/uikit.less";
+@import "./assets/less/theme.less";
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  <nav class="uk-navbar-container" uk-navbar>
+      <div class="uk-navbar-left"><ul class="uk-navbar-nav">
+              <li class="uk-active"><a href="#">Active</a></li>
+              <li>
+                  <a href="#">Parent</a>
+                  <div class="uk-navbar-dropdown">
+                      <ul class="uk-nav uk-navbar-dropdown-nav">
+                          <li class="uk-active"><a href="#">Active</a></li>
+                          <li><a href="#">Item</a></li>
+                          <li><a href="#">Item</a></li>
+                      </ul>
+                  </div>
+              </li>
+              <li><a href="#">Item</a></li>
+          </ul></div>
+  </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
