a84847a9555cf01670a34999abb27d13027f1d02
Pedro de Sá <pedro.trabalhos@outlook.com>
2022-10-10 19:47:03 -0300

    add sell form

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../views/Login.vue'
 import BuyPage from '../views/Buy.vue'
 import CreateUserPage from '../views/CreateUser.vue'
+import SellPage from '../views/Sell.vue'
 
 const routes = [
   { path: '/', name:'Login', component: LoginPage },
   { path: '/compra', name:'Buy', component: BuyPage },
-  { path: '/novoUsuario', name:'CreateUser', component: CreateUserPage}
+  { path: '/novoUsuario', name:'CreateUser', component: CreateUserPage},
+  { path: '/venda', name:'Sell', component: SellPage}
 ]
 
 const router = createRouter({
---
+++ src/views/Sell.vue
@@ -0,0 +1,18 @@
+<template>
+    <NavBar/>
+    <Sell/>
+</template>
+    
+<script>
+    
+import Sell from '@/components/SellForm.vue'
+import NavBar from '@/components/NavBar.vue'
+    
+export default {
+    name: 'SellPage',
+    components: {
+      Sell,
+      NavBar
+    }
+}
+</script>
