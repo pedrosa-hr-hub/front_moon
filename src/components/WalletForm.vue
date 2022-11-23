567b238d67f84b31b71e139f347d7a3d14ae8ff9
Pedro de Sá <pedro.trabalhos@outlook.com>
2022-11-23 20:49:30 -0300

    create func to read a cookie and send data to api

--- src/components/WalletForm.vue
+++ src/components/WalletForm.vue
@@ -6,7 +6,7 @@
   uk-margin-large-right  
   uk-height-max-large">
     
-  <form action="">
+  <form v-on:submit.prevent="submitForm">
     <legend class="uk-legend">Criando Nova Carteira</legend>
 
     <div class="uk-margin">
@@ -14,7 +14,7 @@
       <br>
       <div class="uk-inline">
         <span class="uk-form-icon" uk-icon="icon: user"></span>
-        <input class="uk-input" type="text" placeholder="Seu nome">
+        <input class="uk-input" type="text" placeholder="Seu nome" v-model="form.wallet">
       </div>
     </div>
     <button type="submit" class="uk-button uk-button-primary">CRIAR</button>
@@ -24,7 +24,46 @@
 </template>
 
 <script>
+import router from '@/router';
+import axios from 'axios';
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
 export default {
-  name: 'WalletForm'
+  name: 'WalletForm',
+  data(){
+    return{
+      form: {
+        wallet: '',
+        user: getCookie("user")
+      }
+    }
+  },
+  methods: {
+    chageRoute(route){
+      router.push(route);
+    },
+    alert_error(error){
+      alert(error);
+    },
+    submitForm(){
+      axios.post('http://localhost:3000/wallet', this.form)
+      .then(() => {
+        this.chageRoute('/menu');
+      })
+      .catch((error) => {
+        this.alert_error(error);
+      })
+    }
+  }
 }
 </script>
